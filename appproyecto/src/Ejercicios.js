f16a5f0ae222e538bfcb1cf77cb09515ecebb949
raul <raul.bragado.sanz@iesjulianmarias.es>
2023-05-17 20:27:49 +0200

    Rutas cambiadas

--- appproyecto/src/Ejercicios.js
+++ appproyecto/src/Ejercicios.js
@@ -8,10 +8,10 @@ import { useParams } from "react-router-dom";
 /**RUTAS PARA USARSE EN LOS DIFERENTES ENTORNOS**/
 
 //RUTA DE PRODUCCION
-let ruta = "http://now-exercise.ddns.net/api";
+//let ruta = "http://now-exercise.ddns.net/api";
 
 //RUTA DE DESARROLLO
-//let ruta = "http://localhost/api";
+let ruta = "http://localhost/api";
 
 
 export default class Ejercicios extends React.Component {
