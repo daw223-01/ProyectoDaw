2f94beff4f6d659076d9ef02950628a7ba88c0e8
raul <raul.bragado.sanz@iesjulianmarias.es>
2023-05-19 20:17:15 +0200

    Cambios estilo

--- appproyecto/src/Inicio.js
+++ appproyecto/src/Inicio.js
@@ -5,7 +5,7 @@ export default class Inicio extends React.Component {
 
     randomMessage() {
         // Generar un número aleatorio entre 1 y 4
-        let numeroAleatorio = Math.floor(Math.random() * 4) + 1;
+        let numeroAleatorio = Math.floor(Math.random() * (4 - 1) + 1);
         let msn = "";
         let userName = localStorage.getItem("username");
 
@@ -21,12 +21,9 @@ export default class Inicio extends React.Component {
             case 3:
                 msn = "Bienvenido/a, ";
                 break;
-
-            default:
-                break;
         }
 
-        return msn+userName+"!";
+        return msn + userName + "!";
     }
     render() {
         let mensaje = this.randomMessage();
@@ -34,21 +31,21 @@ export default class Inicio extends React.Component {
         return (
             <div className="container-fluid vh-100 pt-5">
                 <h2>{mensaje}</h2>
-                <div id="canales" className="row h-20">
+                <div id="canales" className="row h-50">
                     <h3>MEJORES RECOMENDACIONES DE YOUTUBE</h3>
-                    <div className="card bg-transparent col-md-4 col-12 border border-light mr-5">
+                    <div className="card bg-transparent col-md-3 col-12 border border-light me-5">
                         <div className="card-body">
                             <h2 className="card-title">Sergio Peinado</h2>
                         </div>
                     </div>
 
-                    <div className="card bg-transparent col-md-4 col-12 border border-light mr-5">
+                    <div className="card bg-transparent col-md-3 col-12 border border-light me-5">
                         <div className="card-body">
                             <h2 className="card-title">Buff Academy</h2>
                         </div>
                     </div>
 
-                    <div className="card bg-transparent col-md-4 col-12 border border-light">
+                    <div className="card bg-transparent col-md-3 col-12 border border-light">
                         <div className="card-body">
                             <h2 className="card-title">Sergio Peinado</h2>
                         </div>
