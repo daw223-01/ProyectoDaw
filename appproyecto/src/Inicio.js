fe9367cadd5b8f8a2a071a0ba0f2bed761503391
raul <raul.bragado.sanz@iesjulianmarias.es>
2023-05-20 11:37:16 +0200

    Cambios en los enlaces

--- appproyecto/src/Inicio.js
+++ appproyecto/src/Inicio.js
@@ -34,35 +34,35 @@ export default class Inicio extends React.Component {
                 <div id="canales" className="row">
                     <h4>MEJORES RECOMENDACIONES DE YOUTUBE</h4>
 
-                    <a className="col-md-3 col-7 me-5" href="https://www.youtube.com/@EntrenaSergioPeinado" target="_blank">
-                        <div className="card shadow bg-transparent border border-2 border-light ">
-                            <div className="card-body">
-                                <h5 className="card-title fst-italic fw-bolder">Sergio Peinado</h5>
-                            </div>
-                            <img class="card-image" src="https://i0.wp.com/www.entrenaconsergiopeinado.com/wp-content/uploads/2015/04/sergio-png.png?fit=300%2C300&ssl=1" width="250" height="250"></img>
+                    <div className="card bg-transparent col-md-3 col-12 border border-2 border-light me-5">
+                        <div className="card-body">
+                            <h5 className="card-title fst-italic fw-bolder">
+                                <a href="https://www.youtube.com/@EntrenaSergioPeinado" target="_blank">Sergio Peinado</a>
+                            </h5>
                         </div>
-                    </a>
+                        <img class="card-image" src="https://i0.wp.com/www.entrenaconsergiopeinado.com/wp-content/uploads/2015/04/sergio-png.png?fit=300%2C300&ssl=1" ></img>
 
-                    <a className="col-md-3 col-7 me-5" href="https://www.youtube.com/GymVirtual" target="_blank">
-                        <div className="card shadow bg-transparent border border-2 border-light">
-                            <div className="card-body">
-                                <h5 className="card-title fst-italic fw-bolder">GymVirtual</h5>
-                            </div>
-                            <img class="card-image" src="https://o.remove.bg/downloads/74c57c3e-cde6-44bc-9bdc-85447d2ee110/15923953322628-removebg-preview.png" width="250" height="250"></img>
+                    </div>
+
+                    <div className="card bg-transparent col-md-3 col-12 border border-2 border-light me-5">
+                        <div className="card-body">
+                            <h5 className="card-title fst-italic fw-bolder">
+                                <a href="https://www.youtube.com/GymVirtual" target="_blank">GymVirtual</a>
+                            </h5>
                         </div>
-                    </a>
+                        <img class="card-image" src="https://o.remove.bg/downloads/74c57c3e-cde6-44bc-9bdc-85447d2ee110/15923953322628-removebg-preview.png"></img>
+                    </div>
 
-                    <a className="col-md-3 col-7 me-5" href="https://www.youtube.com/@buff-academy" target="_blank">
-                        <div className="card shadow bg-transparent border border-2 border-light">
-                            <div className="card-body">
-                                <h5 className="card-title fst-italic fw-bolder">Buff Academy</h5>
-                            </div>
-                            <img class="card-image" src="https://o.remove.bg/downloads/9620866a-0971-4061-b1e9-b319181752e2/90e1b18b8d8932dec6086138ae0ec22f-removebg-preview.png" width="250" height="250"></img>
+                    <div className="card bg-transparent col-md-3 col-12 border border-2 border-light">
+                        <div className="card-body">
+                            <h5 className="card-title fst-italic fw-bolder">
+                                <a href="https://www.youtube.com/@buff-academy" target="_blank">Buff Academy</a>
+                            </h5>
                         </div>
-                    </a>
-
+                        <img class="card-image" src="https://o.remove.bg/downloads/9620866a-0971-4061-b1e9-b319181752e2/90e1b18b8d8932dec6086138ae0ec22f-removebg-preview.png"></img>
+                    </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
